c0ea9b1f5ebe1487209e42e490be17325c64f845
Christian Peralta <christianipc.1987@gmail.com>
2019-03-24 03:41:12 -0300

    termino la configuracion y muestro el contenido de una vista mediante un controlador

---
+++ sistema_de_ventas/src/main/java/com/christian/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.christian.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(path={"/index","/"}, method = RequestMethod.GET)
+	public ModelAndView index(){
+		return new ModelAndView("index");
+	}
+}
